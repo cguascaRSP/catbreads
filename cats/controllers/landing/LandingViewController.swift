2670bb9fd79dbf76f7e0d67efdf5163642cc7181
cguascaRSP <info@redshipping.co>
2025-04-12 16:47:59 -0500

    Initial commit

---
+++ cats/controllers/landing/LandingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LandingViewController.swift
+//  cats
+//
+//  Created by César on 11/04/25.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Catbreeds"
+
+    }
+}
